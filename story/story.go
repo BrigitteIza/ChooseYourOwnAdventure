af2ab09b310f8e6dc384a33a325f7057fcae087a
Mois <BMois@luxoft.com>
2022-01-21 11:14:03 +0200

    add story.go

--- story/story.go
+++ story/story.go
@@ -3,12 +3,40 @@ package story
 import (
 	"encoding/json"
 	"io"
+	"net/http"
 )
+
+var defaultHandlerTempl = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}    </ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+
+}
+
+type handler struct {
+}
 
 type Story map[string]Chapter
 
 type Chapter struct {
-	Title      string   `json:"title"`
+	Title2     string   `json:"title"`
 	Paragraphs []string `json:"paragraphs"`
 	Options    []Option `json:"options"`
 }
